81ef1b80ebf6d297c059f7516bd880b93798d724
sumitpal29 <sumitwithyou@gmail.com>
2021-06-13 01:00:24 +0530

    navbar style fix

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,81 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useCallback, useState } from 'react';
+import { Link } from 'gatsby';
 import _map from 'lodash/map';
-import {navContainer, navLinks, navTitle} from '../scss/navigation.module.scss';
+import cx from 'classNames';
+import { Avatar } from 'antd';
+import { UserOutlined, MenuOutlined } from '@ant-design/icons';
 
-const navLinksArr = [{
+import {
+  navContainer,
+  navLinks,
+  navTitle,
+  navMenuOutline,
+  mobileNavbar,
+  mobileNavbarVisible,
+} from '../scss/navigation.module.scss';
+
+const navLinksArr = [
+  {
     title: 'Home',
-    path: '/'
-}, {
+    path: '/',
+  },
+  {
     title: 'About',
-    path: '/about'
-}, {
+    path: '/about',
+  },
+  {
     title: 'Contact',
-    path: '/contact'
-}]
+    path: '/contact',
+  },
+];
 
 function Header() {
+  const [isMobileNavbarVisible, setMobileNavbarVisibility] = useState(false);
+
+  const handleToggleNavbar = useCallback(() => {
+    setMobileNavbarVisibility((prev) => !prev);
+  }, []);
+
+  const getLinks = useCallback((classname) => {
     return (
-        <nav className={navContainer}>
-            <span className={navTitle}>Sumit Pal</span>
-            <ul className={navLinks}>
-                {_map(navLinksArr, (link, index) => <li key={`nav-link-${index}`}><Link to={link.path}>{link.title}</Link></li>)}
-            </ul>
-        </nav>
-    )
+      <ul className={classname}>
+        {_map(navLinksArr, (link, index) => (
+          <li key={`nav-link-${index}`}>
+            <Link to={link.path}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }, []);
+
+  return (
+    <>
+      <nav className={cx(navContainer)}>
+        <div className={navTitle}>
+          <Avatar
+            style={{ backgroundColor: '#87d068' }}
+            icon={<UserOutlined />}
+            className="m-r-12"
+          />
+          <span>Sumit Pal</span>
+        </div>
+        <div>
+          {getLinks(navLinks)}
+          <MenuOutlined
+            onClick={handleToggleNavbar}
+            className={cx(navMenuOutline, 'm-r-18')}
+          />
+        </div>
+      </nav>
+      <div
+        className={cx(mobileNavbar, {
+          [mobileNavbarVisible]: isMobileNavbarVisible,
+        })}
+      >
+        {getLinks()}
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
