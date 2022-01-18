1f89815f48a7df139cb7d1b76ff1cdaef72d806f
sumitpal29 <sumitwithyou@gmail.com>
2022-01-18 11:17:37 +0530

    Add about me page content

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import cx from 'classnames';
 import Layout from '../../components/Layout';
+import { overview } from './about.module.scss';
 
 function About() {
   return (
     <Layout>
-      <div className="container">
-        <section className='overview'>
-          <h1>About Sumit</h1>
-          <p>some overview of my profile</p>
+      <div className="">
+        <section className={overview}>
+          <div className="flex flex-column container">
+            <h1 className="weigthed-text text-brand-underline-middle">
+              About Sumit
+            </h1>
+            <p className="para-style-grey">
+              I am a software engineer (specialized in Frontend Technologies)
+              based on Bengaluru, India. Currently I am at my hometown, living
+              life like old good days, it gave me the opportunity to work under
+              lap of nature, get inspired from surroundings, it helped me to see
+              and solve problems in different ways. I like creating intuitive
+              web apps by combining the art of design with the art of
+              programming that seamlessly integrate into our lives.
+            </p>
+            <p className="para-style-grey">
+              Other than this, I like to create contents that can help others,
+              write blogs, learn new things & share with you all, and my
+              favourite hobbies are photography and cinematography.
+            </p>
+          </div>
         </section>
         <section>
           <h2>What I do?</h2>
